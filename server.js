4b51ba427844c98864dc7b61373eb0bd186c5880
Destu <destuboroallo@gmail.com>
2023-12-14 06:30:46 +0800

    tes yutub
    Signed-off-by: Destu <destuboroallo@gmail.com>

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {greeting} = require('./user')
 
 app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
-app.get('/youtube', (req,res) => res.send('Hello, UNDIPA'))
+app.get('/youtube', (req,res) => res.send('Hello, yutub'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
